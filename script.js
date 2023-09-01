afd50cb45a001b5331b951320bf55c7fdbac4c48
Christopher Lozzi <clozzi12@gmail.com>
2023-09-01 14:25:19 -0400

    created db json for random joke section + added light/dark display toggle

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", fetchMeal());
 document.addEventListener('DOMContentLoaded', countUp());
+document.addEventListener('DOMContentLoaded', fetchJoke());
 
 
 function fetchMeal() {
@@ -18,4 +19,38 @@ function displayMeal(data) {
 
 function countUp() {
     perSecond = setInterval(function() {counter.innerText++}, 1000)
+}
+
+function fetchJoke() {
+    console.log("hi")
+    fetch ("http://localhost:3000/jokes")
+        .then ((res) => res.json())
+        .then ((data) => displayJoke(data))
+}
+
+function displayJoke(data) {
+    console.log(data[0]["joke"])
+    let joke = document.getElementById('joke')
+    let randomNmbr = Math.floor(Math.random() * 5)
+    joke.textContent = data[randomNmbr]['joke']
+}
+
+darkMode.addEventListener('click', iAmBatman)
+
+ function iAmBatman() {
+    document.body.style.background = "black";
+    //document.getElementById("joke").style.color = 'white';
+    document.querySelectorAll("*").forEach(element => {
+        element.style.color = "gainsboro";
+      });
+}
+
+lightMode.addEventListener('click', hereComesTheSun)
+
+ function hereComesTheSun() {
+    document.body.style.background = "white";
+    //document.getElementById("joke").style.color = 'white';
+    document.querySelectorAll("*").forEach(element => {
+        element.style.color = "black";
+      });
 }
