e3f4d11f4b79b34f6f0745a67223bd809ff58ea3
Christopher Lozzi <clozzi12@gmail.com>
2023-09-14 13:45:57 -0400

    organized code

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-document.addEventListener("DOMContentLoaded", fetchMeal());
-document.addEventListener('DOMContentLoaded', countUp());
+darkMode.addEventListener('click', iAmBatman)
+function iAmBatman() {
+    document.body.style.background = "black";
+    document.querySelectorAll("*").forEach(element => {
+        element.style.color = "gainsboro";
+      });
+}
+
+lightMode.addEventListener('click', hereComesTheSun)
+function hereComesTheSun() {
+    document.body.style.background = "white";
+    document.querySelectorAll("*").forEach(element => {
+        element.style.color = "black";
+      });
+    
+}
+
 document.addEventListener('DOMContentLoaded', fetchJoke());
+function fetchJoke() {
+    fetch ("http://localhost:3000/jokes")
+        .then (res => res.json())
+        .then ((data) => displayJoke(data))
+}
 
+function displayJoke(data) {
+    console.log(data)
+    let joke = document.getElementById('joke')
+    let randomNmbr = Math.floor(Math.random() * 5)
+    joke.textContent = data[randomNmbr]['joke']
+}
 
+document.addEventListener("DOMContentLoaded", fetchMeal());
 function fetchMeal() {
     fetch("https://www.themealdb.com/api/json/v1/1/random.php")
     .then(res => res.json())
@@ -19,43 +46,6 @@ function displayMeal(data) {
     mealPic.src = data.meals[0].strMealThumb;
 }
 
-
-function countUp() {
-    perSecond = setInterval(function() {counter.innerText++}, 1000)
-}
-
-function fetchJoke() {
-    fetch ("http://localhost:3000/jokes")
-        .then (res => res.json())
-        .then ((data) => displayJoke(data))
-}
-
-function displayJoke(data) {
-    console.log(data)
-    let joke = document.getElementById('joke')
-    let randomNmbr = Math.floor(Math.random() * 5)
-    joke.textContent = data[randomNmbr]['joke']
-}
-
-darkMode.addEventListener('click', iAmBatman)
-
- function iAmBatman() {
-    document.body.style.background = "black";
-    document.querySelectorAll("*").forEach(element => {
-        element.style.color = "gainsboro";
-      });
-}
-
-lightMode.addEventListener('click', hereComesTheSun)
-
- function hereComesTheSun() {
-    document.body.style.background = "white";
-    document.querySelectorAll("*").forEach(element => {
-        element.style.color = "black";
-      });
-    
-}
-
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -68,4 +58,9 @@ function drop(ev) {
     ev.preventDefault();
     let data = ev.dataTransfer.getData('text')
     ev.target.appendChild(document.getElementById(data))
+}
+
+document.addEventListener('DOMContentLoaded', countUp());
+function countUp() {
+    perSecond = setInterval(function() {counter.innerText++}, 1000)
 }
